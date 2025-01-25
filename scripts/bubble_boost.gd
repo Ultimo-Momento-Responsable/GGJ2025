29a1fead7a44e1ed5e15665b377ce283f3aa0c15
Julian P. <julian.p950@gmail.com>
2025-01-25 18:31:41 -0300

    feat: add sword behavior

--- scripts/bubble_boost.gd
+++ scripts/bubble_boost.gd
@@ -15,15 +15,19 @@ func _physics_process(delta):
 	# Limitar la velocidad máxima
 	if velocity.length() > max_speed:
 		velocity = velocity.normalized() * max_speed
-		
+
 	move_and_slide()
 	
 	if is_on_wall():
 		for i in range(get_slide_collision_count()):
 			var collision = get_slide_collision(i)
+			var collider = collision.get_collider()
 			var normal = collision.get_normal()  # Obtén la normal de la colisión
 			velocity = velocity.bounce(normal)
 			direction = normal
+			
+			if collider and collider.is_in_group("hazards"):
+				queue_free()
 
 # Función auxiliar para generar un número aleatorio en un rango
 func randf_range(min, max):
--- scripts/sword.gd
+++ scripts/sword.gd
@@ -2,12 +2,34 @@ extends RigidBody3D
 
 @export var base_speed: float = 10.0
 #@export var angular_speed_y: float = 10.0
-@export var angular_speed_z: float = 5.0
+@export var angular_speed_z: float = 10.0
+
+var life_timer: float = 0.0
+var life_time = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	linear_velocity = base_speed * Vector3.UP
+	$CollisionShape3D.disabled = true
+	scale = Vector3(0.0, 0.0, 0.0)
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector3(1.0, 1.0 ,1.0), 2.0)
+	await tween.finished
+	$CollisionShape3D.disabled = false
+	# linear_velocity = base_speed * Vector3.UP
 	angular_velocity = angular_speed_z * Vector3.BACK
+
+func _process(delta):
+	life_timer += delta
+	if life_timer >= life_time:
+		destroy()
 
 func _on_body_entered(body:Node):
 	print(body, " entered")
+	
+func destroy():
+	life_timer = 0.0
+	$CollisionShape3D.disabled = true
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector3(0.1, 0.1 ,0.1), 0.2)
+	await tween.finished
+	queue_free()
