d173048947856fbc098ffa7a4499e5e6af6886a1
Andrés Matterson <andresmatter@hotmail.com>
2025-01-26 00:42:20 -0300

    feat: Add spike_ball death tween

--- scripts/spike_ball.gd
+++ scripts/spike_ball.gd
@@ -5,12 +5,21 @@ extends RigidBody3D
 var start_direction = Vector3.ZERO
 
 func _ready() -> void:
+	$CollisionShape3D.disabled = true
+	scale = Vector3(0.0, 0.0, 0.0)
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector3(1.0, 1.0 ,1.0), 2.0)
+	await tween.finished
+	$CollisionShape3D.disabled = false
 	start_direction = Vector3(randf_range(-1, 1), randf_range(-1, 1), 0).normalized()
 	linear_velocity = start_direction * speed
 
 func _process(delta: float) -> void:
 	if remaining_time <= 0:
+		$CollisionShape3D.disabled = true
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector3(0.1, 0.1 ,0.1), 0.2)
+		await tween.finished
 		queue_free()
 		return
-		
 	remaining_time -= delta
